2f20c988efd4a386997ae51057640c18c7ec437f
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-06-25 22:41:16 +0600

    project basic setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import PublicProfilePage from "./pages/PublicProfilePage";
+import StorePage from "./pages/StorePage";
+import CartPage from "./pages/CartPage";
+import FriendPage from "./pages/FriendPage";
+import NewsfeedPage from "./pages/NewsfeedPage";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<PublicProfilePage />} />
+      <Route path="/store" element={<StorePage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/friend" element={<FriendPage />} />
+      <Route path="/newsfeed" element={<NewsfeedPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/PublicProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PublicProfilePage = () => {
+  return (
+    <div>
+      public page
+    </div>
+  )
+}
+
+export default PublicProfilePage
