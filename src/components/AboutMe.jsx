7f43eaf06cdc46f84bd07c89d4b15d87122ad4ec
AJOY SAHA <ajoy.code.dreamer@gmail.com>
2025-06-28 19:08:45 +0600

    added about me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className='max-w-[283px] pl-[27px] pr-[26px] py-[29px] bg-white rounded-[15px] font-"Poppins"'>
+      <div className="flex items-center justify-between mb-[29px] font-bold text-[#3E3F5E] text-sm">
+        <h3>About Me</h3>
+        <span>...</span>
+      </div>
+      <p className="text-sm text-[#AFB0C0] font-normal">
+        Hi! My name is A B M Shawon Islam but some people may know me as
+        GameHuntress! I have a Twitch channel where I stream, play and review
+        all the newest games.
+      </p>
+      <div className="font-bold text-[#AFB0C0] text-xs">
+        <h5 className="mt-[18px]">
+          Joined:<span className="ml-[25px] font-normal">22 November 2008</span>
+        </h5>
+        <h5 className="mt-[18px]">
+          City:<span className="ml-[25px] font-normal">Dhaka</span>
+        </h5>
+        <h5 className="mt-[18px]">
+          City:<span className="ml-[25px] font-normal"> 18 years</span>
+        </h5>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
--- src/pages/PublicProfilePage.jsx
+++ src/pages/PublicProfilePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ProfileBanner from '../components/ProfileBanner'
 import Container from '../components/commonComponents/Container'
+import AboutMe from '../components/AboutMe'
 
 const PublicProfilePage = () => {
   return (
     <Container>
       <div>
       <ProfileBanner/>
+      </div>
+      <div className='flex mt-[33px]'>
+        <AboutMe/>
       </div>
     </Container>
   )
